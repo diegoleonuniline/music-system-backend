a88e9d47f3a0d9e71dc1dd61fb42093d9122aae5
diegoleonuniline <desarrollos@uniline.mx>
2026-01-03 13:44:20 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -36,9 +36,9 @@ app.use('/api/events', require('./routes/events'));
 app.use('/api/rehearsals', require('./routes/rehearsals'));
 app.use('/api/song-resources', require('./routes/song-resources'));
 app.use('/api/notifications', require('./routes/notifications'));
-app.use('/api/artists', require('./routes/artists'));
-app.use('/api/projects', require('./routes/projects'));
-app.use('/api/song-settings', require('./routes/song-settings'));
+//app.use('/api/artists', require('./routes/artists'));
+//app.use('/api/projects', require('./routes/projects'));
+//app.use('/api/song-settings', require('./routes/song-settings'));
 
 // Ruta de prueba
 app.get('/', (req, res) => {

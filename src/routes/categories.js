bd442f608364939cdf14758a44154d9b5fa85e7f
diegoleonuniline <desarrollos@uniline.mx>
2026-01-04 12:12:49 -0600

    Update categories.js

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -48,6 +48,7 @@ router.put('/:id', verifyToken, isGroupAdmin, async (req, res) => {
 // Eliminar categoría
 router.delete('/:id', verifyToken, isGroupAdmin, async (req, res) => {
   try {
+    await db.query('UPDATE songs SET category_id = NULL WHERE category_id = ? AND group_id = ?', [req.params.id, req.user.group_id]);
     await db.query('DELETE FROM categories WHERE id = ? AND group_id = ?', [req.params.id, req.user.group_id]);
     res.json({ message: 'Categoría eliminada' });
   } catch (error) {

f7cdb862a6a9aba339fc4f2c9801c53ab5591850
diegoleonuniline <desarrollos@uniline.mx>
2026-01-20 13:43:18 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const fetch = require('node-fetch');
 require('dotenv').config();
 const app = express();
-
 // CORS
 app.use(cors({
     origin: [
